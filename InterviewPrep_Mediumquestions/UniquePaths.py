356e4304529876847b3fee2d154203991c5fbbd4
Lalitha Siva Priya Amudalapally <lallu1201@gmail.com>
2020-06-01 11:41:22 -0700

    Add files via upload

---
+++ InterviewPrep_Mediumquestions/UniquePaths.py
@@ -0,0 +1,16 @@
+https://leetcode.com/explore/interview/card/top-interview-questions-medium/111/dynamic-programming/808/
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        
+        
+        dp = [ [1] * n for _ in range(m) ]
+        
+        for i in range(1,m):
+            
+            for j in range(1,n):
+                
+                dp[i][j] = dp[i][j-1] + dp[i-1][j]
+                
+        
+        return dp[-1][-1]
